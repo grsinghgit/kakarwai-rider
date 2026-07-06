88eaf3d5b86a38d0b884ee5a8216b11785e2fb70
grsinghgit <grsinghgit@gmail.com>
2026-07-06 17:16:49 +0530

    splash activity added working

---
+++ app/src/main/java/com/gr/kakarwairider/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.gr.kakarwairider
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // ✅ 5 Second Delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)  // 5 seconds
+    }
+}
